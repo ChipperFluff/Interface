1ca7ccc3e2880165bc55a207878c0b0a36e71966
Dominik Krenn <dominik.krenn@1tool.com>
2025-05-26 13:48:44 +0200

    feat: enhance a_dd action with color-coded output for better readability

--- action/a_dd.java
+++ action/a_dd.java
@@ -3,8 +3,10 @@ package Interface.action;
 import Interface.Action;
 import Interface.Interface;
 import Interface.View;
-import java.util.Map;
+import Interface.Color;
+
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class a_dd extends Action {
@@ -16,24 +18,36 @@ public class a_dd extends Action {
 
     @Override
     public Boolean execute(Interface base) {
-        System.out.println("===== Dump & Die =====");
+        System.out.println(Color.ANSI_BLUE + "========== DUMP & DIE ==========" + Color.ANSI_RESET);
 
         for (int i = 0; i < data.length; i++) {
-            System.out.println("[" + i + "] => " + formatValue(data[i]));
+            Object item = data[i];
+            String type = (item == null) ? "null" : item.getClass().getSimpleName();
+            String typeColor = getTypeColor(type);
+
+            System.out.println(
+                Color.ANSI_WHITE + "[" + i + "] " +
+                typeColor + "(" + type + ")" +
+                Color.ANSI_RESET + " => " + formatValue(item)
+            );
         }
 
-        System.out.println("============ END ============");
+        System.out.println(Color.ANSI_BLUE + "============ END ============" + Color.ANSI_RESET);
         return false;
     }
 
     private String formatValue(Object obj) {
-        if (obj == null) return "null";
+        if (obj == null) return Color.ANSI_RED + "null" + Color.ANSI_RESET;
 
         if (obj instanceof Map) {
             StringBuilder sb = new StringBuilder("Map {\n");
-            ((Map<?, ?>) obj).forEach((k, v) ->
-                sb.append("  ").append(k).append(" => ").append(formatValue(v)).append("\n")
-            );
+            ((Map<?, ?>) obj).forEach((k, v) -> {
+                String type = v == null ? "null" : v.getClass().getSimpleName();
+                String color = getTypeColor(type);
+                sb.append("  ").append(k).append(" => ")
+                  .append(color).append("(").append(type).append(") ")
+                  .append(formatValue(v)).append(Color.ANSI_RESET).append("\n");
+            });
             sb.append("}");
             return sb.toString();
         }
@@ -42,7 +56,11 @@ public class a_dd extends Action {
             StringBuilder sb = new StringBuilder("List [\n");
             int i = 0;
             for (Object val : (List<?>) obj) {
-                sb.append("  ").append(i++).append(" => ").append(formatValue(val)).append("\n");
+                String type = val == null ? "null" : val.getClass().getSimpleName();
+                String color = getTypeColor(type);
+                sb.append("  ").append(i++).append(" => ")
+                  .append(color).append("(").append(type).append(") ")
+                  .append(formatValue(val)).append(Color.ANSI_RESET).append("\n");
             }
             sb.append("]");
             return sb.toString();
@@ -51,7 +69,11 @@ public class a_dd extends Action {
         if (obj instanceof Set) {
             StringBuilder sb = new StringBuilder("Set {\n");
             for (Object val : (Set<?>) obj) {
-                sb.append("  ").append(formatValue(val)).append("\n");
+                String type = val == null ? "null" : val.getClass().getSimpleName();
+                String color = getTypeColor(type);
+                sb.append("  ")
+                  .append(color).append("(").append(type).append(") ")
+                  .append(formatValue(val)).append(Color.ANSI_RESET).append("\n");
             }
             sb.append("}");
             return sb.toString();
@@ -61,12 +83,37 @@ public class a_dd extends Action {
             StringBuilder sb = new StringBuilder("Array [\n");
             Object[] arr = (Object[]) obj;
             for (int i = 0; i < arr.length; i++) {
-                sb.append("  ").append(i).append(" => ").append(formatValue(arr[i])).append("\n");
+                Object val = arr[i];
+                String type = val == null ? "null" : val.getClass().getSimpleName();
+                String color = getTypeColor(type);
+                sb.append("  ").append(i).append(" => ")
+                  .append(color).append("(").append(type).append(") ")
+                  .append(formatValue(val)).append(Color.ANSI_RESET).append("\n");
             }
             sb.append("]");
             return sb.toString();
         }
 
         return obj.toString();
+    }
+
+    private String getTypeColor(String type) {
+        if (type == null) return Color.ANSI_RED;
+
+        switch (type) {
+            case "String": return Color.ANSI_GREEN;
+            case "Integer":
+            case "Long":
+            case "Double":
+            case "Float":
+            case "Short":
+            case "Byte": return Color.ANSI_YELLOW;
+            case "Boolean": return Color.ANSI_CYAN;
+            case "Map": return Color.ANSI_PURPLE;
+            case "List": return Color.ANSI_BLUE;
+            case "Set": return Color.ANSI_RED;
+            case "Object[]": return Color.ANSI_WHITE;
+            default: return Color.ANSI_WHITE;
+        }
     }
 }
